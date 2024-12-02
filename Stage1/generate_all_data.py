d8458369f74911aac635f39c8a00f487688db32c
Soclofa <95235570+Soclofa@users.noreply.github.com>
2024-12-02 18:17:40 +0200

    Update generate_all_data.py

--- Stage1/generate_all_data.py
+++ Stage1/generate_all_data.py
@@ -30,6 +30,75 @@ def random_date(start_year=2020, end_year=2025):
     end_date = datetime(end_year, 12, 31)
     return fake.date_between_dates(date_start=start_date, date_end=end_date)
 
+# Insert data into Insurance table
+for i in range(200000):
+    insurance_type = random.choice(insurance_types)
+    start_date = random_date()
+    end_date = start_date + timedelta(days=random.randint(30, 365))
+    covered_amount = round(random.uniform(1000, 50000), 2)
+    cursor.execute("""
+        INSERT INTO Insurance (CoveredAmount, StartDate, EndDate)
+        VALUES (%s, %s, %s)
+    """, (covered_amount, start_date, end_date))
+
+# Insert data into Subscription table
+for i in range(100000):
+   
+    subscription_type = random.choice(subscription_types)
+    renewal_date = random_date()
+    purchase_date = renewal_date - timedelta(days=random.randint(30, 365))
+    cursor.execute("""
+        INSERT INTO Subscription (SubscriptionType, RenewalDate, PurchaseDate)
+        VALUES (%s, %s, %s)
+    """, ( subscription_type, renewal_date, purchase_date))
+
+for i in range(200000):
+    subscription_id_index = random.randint(1,99999)
+
+    cursor.execute("""
+        INSERT INTO Reader (SubscriptionID)
+        VALUES (%s)
+    """, [subscription_id_index])
+
+for i in range(50000):
+
+    subscription_id_index = random.randint(1,99999)
+    penalty_id = random.randint(1,200000)
+
+    book_id = i + 1
+    cursor.execute("""
+        INSERT INTO SubscriptionPenalties (SubscriptionID, PenaltyID, BookID)
+        VALUES (%s, %s, %s)
+    """, (subscription_id_index, penalty_id, book_id))
+
+# Insert data into Wage table
+for i in range(200000):
+    amount = round(random.uniform(1000, 10000), 2)
+    pay_day = random_date(2020, 2024)
+    cursor.execute("""
+        INSERT INTO Wage (Amount, PayDay)
+        VALUES (%s, %s)
+    """, (amount, pay_day))
+
+    wage_id = i + 1
+    cursor.execute("""
+        INSERT INTO Employee (WageID)
+        VALUES (%s)
+    """, (wage_id,))
+
+connection.commit()
+
+# Insert data into Procurement table
+for i in range(1000):
+    procurement_id = i+1
+    source = fake.company()
+    procurement_date = random_date()
+    cost = round(random.uniform(500, 10000), 2)
+    cursor.execute("""
+        INSERT INTO Procurement (Source, ProcurementDate, Cost)
+        VALUES (%s, %s, %s)
+    """, (source, procurement_date, cost))
+connection.commit()
 
 for i in range(200000):
     procurement_id = random.randint(1,999)
