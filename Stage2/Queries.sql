05cb6db08dca50fe97c1198f9556c972f7c2bd18
Soclofa <95235570+Soclofa@users.noreply.github.com>
2024-12-23 14:11:20 +0200

    Add queries, paramqueries, their respective logs

---
+++ Stage2/Queries.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cf1ad82437bde6df838148844b1ca9e621400c686ad308f2768b3800f2f0c251
+size 3144
---
+++ Stage2/indices.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7baa92c3769efd52a543ff743d6b1f880f1286a8d6860ebbb280e4eb38ccea85
+size 796
