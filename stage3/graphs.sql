6e16e9158ede0389dbfb0423b08bf4dcbb277ee0
Moshe Kravitz <moshe.kravitz@appricots.onmicrosoft.com>
2024-12-30 10:55:48 +0200

    added logs for graphs

--- stage3/graphs.sql
+++ stage3/graphs.sql
@@ -1,12 +1,26 @@
 -- graph 1: subscription types
 SELECT tier, count(*) tier_count
-FROM subscription 
+FROM subscription
 group by tier
 ORDER BY
     tier_count DESC;
+   tier   | tier_count
+----------+------------
+ Premium  |      40460
+ Family   |      40034
+ Elite    |      39980
+ Standard |      39786
+ Basic    |      39741
+(5 rows)
 
 -- graph 2: payday
-SELECT monthly_payment_date, count(*) mpd_count
-FROM wage 
+ SELECT monthly_payment_date, count(*) mpd_count
+FROM wage
 group by monthly_payment_date
 ORDER BY mpd_count DESC;
+ monthly_payment_date | mpd_count
+----------------------+-----------
+                   10 |        49
+                   25 |        30
+                    1 |        21
+(3 rows)
